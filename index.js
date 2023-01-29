7c86238d04e1dc9ec55b89d4ab494a3af16cada8
peterbujaky <peterbujaky@gmail.com>
2023-01-29 11:49:07 +0100

    Add chart using chart.js

--- index.js
+++ index.js
@@ -2,4 +2,33 @@ fetch("./data.json")
 .then(res => res.json())
 .then(data => {
     console.log(data)
+    let days = data.map(item => item.day);
+    let amounts = data.map(item => item.amount);
+
+    const ctx = document.getElementById("myChart").getContext("2d");
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: days,
+      datasets: [{
+        label: '# of Votes',
+        data: amounts,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
 });
+
+
+
